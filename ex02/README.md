bd8eea58a0eebed7cf2ca016966a18e8505c794d
kopalych <kopalychmail@gmail.com>
2025-12-07 00:42:48 +0100

    ex02

---
+++ ex02/README.md
@@ -0,0 +1,88 @@
+
+Tests:
+10 iterations
+
+==================================================
+Test:  Non-existant text article1
+==================================================
+
+Boyer Moore Search   10 inerations
+MIN: 0.0006 seconds
+MAX: 0.0007 seconds
+AVR: 0.0007 seconds
+SUM: 0.0065 seconds
+
+KMP Search           10 inerations
+MIN: 0.0006 seconds
+MAX: 0.0007 seconds
+AVR: 0.0007 seconds
+SUM: 0.0065 seconds
+
+Rabin Karp Search    10 inerations
+MIN: 0.0016 seconds
+MAX: 0.0017 seconds
+AVR: 0.0017 seconds
+SUM: 0.0165 seconds
+
+==================================================
+Test:  Non-existant text article2
+==================================================
+Boyer Moore Search   10 inerations
+MIN: 0.0006 seconds
+MAX: 0.0007 seconds
+AVR: 0.0006 seconds
+SUM: 0.0063 seconds
+
+KMP Search           10 inerations
+MIN: 0.0006 seconds
+MAX: 0.0007 seconds
+AVR: 0.0007 seconds
+SUM: 0.0066 seconds
+
+Rabin Karp Search    10 inerations
+MIN: 0.0016 seconds
+MAX: 0.0021 seconds
+AVR: 0.0017 seconds
+SUM: 0.0172 seconds
+
+==================================================
+Test:  Basic string article1
+==================================================
+Boyer Moore Search   10 inerations
+MIN: 0.0001 seconds
+MAX: 0.0001 seconds
+AVR: 0.0001 seconds
+SUM: 0.0006 seconds
+
+KMP Search           10 inerations
+MIN: 0.0003 seconds
+MAX: 0.0004 seconds
+AVR: 0.0003 seconds
+SUM: 0.0034 seconds
+
+Rabin Karp Search    10 inerations
+MIN: 0.0009 seconds
+MAX: 0.0009 seconds
+AVR: 0.0009 seconds
+SUM: 0.0087 seconds
+
+==================================================
+Test:  Basic string article2
+==================================================
+Boyer Moore Search   10 inerations
+MIN: 0.0002 seconds
+MAX: 0.0002 seconds
+AVR: 0.0002 seconds
+SUM: 0.0021 seconds
+
+KMP Search           10 inerations
+MIN: 0.0007 seconds
+MAX: 0.0007 seconds
+AVR: 0.0007 seconds
+SUM: 0.0066 seconds
+
+Rabin Karp Search    10 inerations
+MIN: 0.0017 seconds
+MAX: 0.0020 seconds
+AVR: 0.0017 seconds
+SUM: 0.0170 seconds
---
+++ ex02/main.py
@@ -0,0 +1,167 @@
+from timeit import repeat
+from typing import Callable
+
+
+def build_shift_table(pattern):
+    table = {}
+    length = len(pattern)
+    # Для кожного символу в підрядку встановлюємо зсув рівний довжині підрядка
+    for index, char in enumerate(pattern[:-1]):
+        table[char] = length - index - 1
+    # Якщо символу немає в таблиці, зсув буде дорівнювати довжині підрядка
+    table.setdefault(pattern[-1], length)
+    return table
+
+def boyer_moore_search(text, pattern):
+    # Створюємо таблицю зсувів для патерну (підрядка)
+    shift_table = build_shift_table(pattern)
+    i = 0 # Ініціалізуємо початковий індекс для основного тексту
+
+    # Проходимо по основному тексту, порівнюючи з підрядком
+    while i <= len(text) - len(pattern):
+        j = len(pattern) - 1 # Починаємо з кінця підрядка
+
+        # Порівнюємо символи від кінця підрядка до його початку
+        while j >= 0 and text[i + j] == pattern[j]:
+            j -= 1 # Зсуваємось до початку підрядка
+
+        # Якщо весь підрядок збігається, повертаємо його позицію в тексті
+        if j < 0:
+            return i # Підрядок знайдено
+
+        # Зсуваємо індекс i на основі таблиці зсувів
+        # Це дозволяє "перестрибувати" над неспівпадаючими частинами тексту
+        i += shift_table.get(text[i + len(pattern) - 1], len(pattern))
+
+    # Якщо підрядок не знайдено, повертаємо -1
+    return -1
+
+
+def compute_lps(pattern):
+    lps = [0] * len(pattern)
+    length = 0
+    i = 1
+
+    while i < len(pattern):
+        if pattern[i] == pattern[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+
+    return lps
+
+def kmp_search(main_string, pattern):
+    M = len(pattern)
+    N = len(main_string)
+
+    lps = compute_lps(pattern)
+
+    i = j = 0
+
+    while i < N:
+        if pattern[j] == main_string[i]:
+            i += 1
+            j += 1
+        elif j != 0:
+            j = lps[j - 1]
+        else:
+            i += 1
+
+        if j == M:
+            return i - j
+
+    return -1  # якщо підрядок не знайдено
+
+
+def polynomial_hash(s, base=256, modulus=101):
+    """
+    Повертає поліноміальний хеш рядка s.
+    """
+    n = len(s)
+    hash_value = 0
+    for i, char in enumerate(s):
+        power_of_base = pow(base, n - i - 1) % modulus
+        hash_value = (hash_value + ord(char) * power_of_base) % modulus
+    return hash_value
+
+def rabin_karp_search(main_string, substring):
+    # Довжини основного рядка та підрядка пошуку
+    substring_length = len(substring)
+    main_string_length = len(main_string)
+    
+    # Базове число для хешування та модуль
+    base = 256 
+    modulus = 101  
+    
+    # Хеш-значення для підрядка пошуку та поточного відрізка в основному рядку
+    substring_hash = polynomial_hash(substring, base, modulus)
+    current_slice_hash = polynomial_hash(main_string[:substring_length], base, modulus)
+    
+    # Попереднє значення для перерахунку хешу
+    h_multiplier = pow(base, substring_length - 1) % modulus
+    
+    # Проходимо крізь основний рядок
+    for i in range(main_string_length - substring_length + 1):
+        if substring_hash == current_slice_hash:
+            if main_string[i:i+substring_length] == substring:
+                return i
+
+        if i < main_string_length - substring_length:
+            current_slice_hash = (current_slice_hash - ord(main_string[i]) * h_multiplier) % modulus
+            current_slice_hash = (current_slice_hash * base + ord(main_string[i + substring_length])) % modulus
+            if current_slice_hash < 0:
+                current_slice_hash += modulus
+
+    return -1
+
+
+def test(string: str, substring: str, func: Callable, name: str):
+
+    REPEAT_TIMES = 10
+
+    times = repeat(
+        lambda: func(string, substring),
+        repeat=REPEAT_TIMES,
+        number=1
+    )
+    print(f"{name}".ljust(20), f"{REPEAT_TIMES} inerations")
+
+    print(f"MIN: {min(times):.4f} seconds")
+    print(f"MAX: {max(times):.4f} seconds")
+    print(f"AVR: {sum(times)/len(times):.4f} seconds")
+    print(f"SUM: {sum(times):.4f} seconds")
+    print()
+
+
+def main():
+
+    with open("article1.txt") as f:
+        article1 = f.read()
+
+    with open("article1.txt") as f:
+        article2 = f.read()
+
+    tests = {
+        "Non-existant text article1": (article1, "123"),
+        "Non-existant text article2": (article2, "123"),
+        "Basic string article1": (article1, "interpolationSearch"),
+        "Basic string article2": (article2, "linked list"),
+    }
+    for name in tests:
+        print('=' * 50)
+        print(f"Test:  {name}")
+        print('=' * 50)
+        test(tests[name][0], tests[name][1], boyer_moore_search, "Boyer Moore Search")
+        test(tests[name][0], tests[name][1], kmp_search, "KMP Search")
+        test(tests[name][0], tests[name][1], rabin_karp_search, "Rabin Karp Search")
+        print()
+
+
+if __name__ == "__main__":
+    main()
